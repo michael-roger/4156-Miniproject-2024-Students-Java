1c145e984b4db6485ed7f5d689727df257c73f74
Michael Roger <35877543+michael-roger@users.noreply.github.com>
2024-09-17 16:44:12 -0400

    Round1 (#1)
    * Round 1 - Fixed all style errors.  Fixed all PMD errors.  Fixed some bugs.
    
    * Round 1 - Update
    
    * Round 1 - Update
    
    * Round 1 - Update
    
    * Round 1 - Fix more bugs
    
    * Round 1 - Fix more bugs

--- IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
+++ IndividualProject/src/test/java/dev/coms4156/project/individualproject/CourseUnitTests.java
@@ -1,11 +1,17 @@
 package dev.coms4156.project.individualproject;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * The unit tests for all methods in the Course.java class
+ *
+ * @see Course
+ */
 @SpringBootTest
 @ContextConfiguration
 public class CourseUnitTests {
@@ -14,7 +20,6 @@ public class CourseUnitTests {
   public static void setupCourseForTesting() {
     testCourse = new Course("Griffin Newbold", "417 IAB", "11:40-12:55", 250);
   }
-
 
   @Test
   public void toStringTest() {
@@ -25,4 +30,3 @@ public class CourseUnitTests {
   /** The test course instance used for testing. */
   public static Course testCourse;
 }
-
